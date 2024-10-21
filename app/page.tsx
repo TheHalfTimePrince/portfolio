17616bc6abf5568b980559554f13daef725b873c
TheHalfTimePrince <ziggybaker123@gmail.com>
2024-10-21 07:09:23 +0100

    refresh

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,85 @@
+"use client";
+
+import { Switch } from "@/components/ui/switch";
+import { ThemeProvider, useTheme } from "next-themes";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const { theme, setTheme } = useTheme();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+      <div className="w-full flex flex-col space-y-16 max-w-screen-xl">
+        <div className="aspect-video max-h-[40rem] w-full relative">
+          <div className="absolute inset-0 bg-gradient-to-t from-background/70 to-transparent dark:from-transparent dark:to-background/70 z-10"></div>
+          <Image
+            src="/hero.png"
+            alt="logo"
+            fill
+            className="object-cover dark:invert "
+            style={{
+              maskImage:
+                "radial-gradient(ellipse 70% 70% at 50% 50%, black 20%, transparent 80%)",
+              WebkitMaskImage:
+                "radial-gradient(ellipse 70% 70% at 50% 50%, black 20%, transparent 80%)",
+            }}
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <section className="px-4">
+          <div className="flex space-x-4 items-center">
+            <h1 className="text-4xl font-regular mb-4">Ziggy Baker</h1>
+            <Switch
+              className="mb-1"
+              checked={theme === "dark"}
+              onCheckedChange={(checked) =>
+                setTheme(checked ? "dark" : "light")
+              }
+            />
+          </div>
+          <p className="text-sm mb-8">Web Developer</p>
+          <p className="mb-8">
+            I specialize in developing multi-tenant full stack Next.js
+            applications for SaaS, with a focus on single directory structures
+            and unified hosting solutions. 
+          </p>
+          <p className="text-xs mb-8 font-medium font-mono ">
+            typescript · javascript · next.js · react · tailwind · sass ·
+            postgres · drizzle · nosql · clickhouse · docker · firebase ·
+            multi-tenancy · express · django · python · selenium · pil{" "}
+          </p>
+
+          <a
+            href="#contact"
+            className="bg-primary text-primary-foreground px-6 py-2 rounded-md hover:bg-primary/90 transition-colors"
+          >
+            Contact Me
+          </a>
+        </section>
+
+        <section className="px-4 pb-32">
+          <h2 className="text-3xl font-bold mb-6">Projects</h2>
+          
+          <div className="space-y-6">
+            <div>
+              <h3 className="text-xl font-semibold mb-3">Private Repos</h3>
+              <ul className="list-disc list-inside space-y-2">
+                <li>Api & Usage based SaaS starter</li>
+                <li>Api & Membership w/ rate limiting SaaS starter</li>
+                <li>Team & Membership based SaaS starter</li>
+              </ul>
+            </div>
+            
+            <div>
+              <h3 className="text-xl font-semibold mb-3">Public Repos</h3>
+              <ul className="list-disc list-inside space-y-2">
+                <li>Api & Token based SaaS starter</li>
+                <li>Easy Api's - Api's as a service</li>
+                <li>Dark Step - Audio Based Step Sequencer</li>
+                <li>Responsive Web Design templates</li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </div>
   );
 }
